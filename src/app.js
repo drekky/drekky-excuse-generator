fe706daac8c6ade3bec3afc9692b02244c8b729c
drekky <drewleontis@gmail.com>
2023-10-05 20:23:00 -0400

    Finished JS Excuse Generator Project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['Peter', 'Lois', 'Stewy', 'Chris', 'Brian'];
+  let action = ['took', 'ran off with', 'ripped up', 'puked on', 'lost'];
+  let what = ['my car', 'my laptop', 'the keys', 'my homework', 'my clothes'];
+  let when = ['yesterday', 'today', 'last night', 'an hour ago', '5 minutes ago'];
+
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+
+  let excuse = randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen + "!";
+  document.getElementById("excuse").textContent = excuse;
+
+
+  const button = document.getElementById("btn");
+
+  button.addEventListener("click", () => {
+    randomWho = who[Math.floor(Math.random() * who.length)];
+    randomAction = action[Math.floor(Math.random() * action.length)];
+    randomWhat = what[Math.floor(Math.random() * what.length)];
+    randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    excuse = randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen + "!";
+    document.getElementById("excuse").textContent = excuse;
+  
+  });
 };
